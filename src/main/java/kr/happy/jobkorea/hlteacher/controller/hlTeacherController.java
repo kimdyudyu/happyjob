163bb78449fb3e03c54eb708948d2fca16836cc5
HappyJob <HappyJob@DESKTOP-6FL46AR>
2020-06-18 09:29:59 +0900

    테스트 커밋

---
+++ src/main/java/kr/happy/jobkorea/hlteacher/controller/hlTeacherController.java
@@ -0,0 +1,5 @@
+package kr.happy.jobkorea.hlteacher.controller;
+
+public class hlTeacherController {
+	//ㅁㄴㄹㅇ;ㅣㄴ마ㅗㄹㄴ미ㅏ론;ㅏ
+}
---
+++ src/main/java/kr/happy/jobkorea/hlteacher/service/hlTeacherService.java
@@ -0,0 +1,6 @@
+package kr.happy.jobkorea.hlteacher.service;
+
+public interface hlTeacherService {
+	// 임시테스트
+	//ㅁㄴㅇㄹㅇㄴㅁ리ㅏㅇㄴ모리ㅏㅗㅇㄴ;ㅣㅏㄹ
+}
