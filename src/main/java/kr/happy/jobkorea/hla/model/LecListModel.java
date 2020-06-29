b9163e286f8148321d01decc6776c340d290fcec
arcmee <kihyun3297@gmail.com>
2020-06-29 15:36:42 +0900

    승현 커밋

---
+++ src/main/java/kr/happy/jobkorea/hla/model/LecListModel.java
@@ -0,0 +1,86 @@
+package kr.happy.jobkorea.hla.model;
+
+import java.util.Date;
+
+
+public class LecListModel {
+
+	private String no;
+	private String loginID;
+	private String title;
+	private String st_date;
+	private String ed_date;
+	private String name;
+	private String email;
+	private String area;
+	private String area_name;
+	
+	
+	
+	
+	
+	
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getArea() {
+		return area;
+	}
+	public void setArea(String area) {
+		this.area = area;
+	}
+	public String getArea_name() {
+		return area_name;
+	}
+	public void setArea_name(String area_name) {
+		this.area_name = area_name;
+	}
+	public String getNo() {
+		return no;
+	}
+	public void setNo(String no) {
+		this.no = no;
+	}
+	public String getLoginID() {
+		return loginID;
+	}
+	public void setLoginID(String loginID) {
+		this.loginID = loginID;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getSt_date() {
+		return st_date;
+	}
+	public void setSt_date(String st_date) {
+		this.st_date = st_date;
+	}
+	public String getEd_date() {
+		return ed_date;
+	}
+	public void setEd_date(String ed_date) {
+		this.ed_date = ed_date;
+	}
+	
+	
+	
+	
+
+	
+	
+}
